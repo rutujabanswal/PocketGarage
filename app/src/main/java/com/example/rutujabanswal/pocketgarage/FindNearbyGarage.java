8c125c2c2da7a35a9da4c46517808ed57836d38c
Rutuja Banswal <rutuja.banswal@gmail.com>
2018-03-14 16:33:00 +0530

    Plotted Garages,Car Service Centers and Added Service Activity

--- app/src/main/java/com/example/rutujabanswal/pocketgarage/FindNearbyGarage.java
+++ app/src/main/java/com/example/rutujabanswal/pocketgarage/FindNearbyGarage.java
@@ -1,21 +1,29 @@
 package com.example.rutujabanswal.pocketgarage;
 
 
+import android.content.res.Configuration;
+import android.location.Location;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import static com.example.rutujabanswal.pocketgarage.R.id.*;
+import static com.example.rutujabanswal.pocketgarage.R.id.map1;
 
 
 /**
@@ -24,6 +32,7 @@ import static com.example.rutujabanswal.pocketgarage.R.id.*;
 public class FindNearbyGarage extends Fragment implements OnMapReadyCallback {
 
 
+    private int PROXIMITY_RADIUS = 10000;
     public FindNearbyGarage() {
         // Required empty public constructor
     }
@@ -33,20 +42,114 @@ public class FindNearbyGarage extends Fragment implements OnMapReadyCallback {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =inflater.inflate(R.layout.fragment_find_nearby_garage, container, false);
+        View view = inflater.inflate(R.layout.fragment_find_nearby_garage, container, false);
         FragmentManager fm = getChildFragmentManager();
-        SupportMapFragment mapFragment = (SupportMapFragment)fm.findFragmentById(map1);
+        SupportMapFragment mapFragment = (SupportMapFragment) fm.findFragmentById(map1);
         mapFragment.getMapAsync(this);
 
-    return view;
+        return view;
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        GoogleMap map =googleMap;
-        LatLng aurangabad = new LatLng(19.8762, 75.3433);
-        map.addMarker(new MarkerOptions().position(aurangabad)
-                .title("Marker in Aurangabad"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(aurangabad));
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
     }
+
+
+ GoogleMap mMap;
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.mMap = googleMap;
+
+        final double latitude = 19.8762;
+        final double longitude = 75.3433;
+        final String FuelStation = "gas_station";
+        final String CarRepaire = "car_repair";
+        // create marker
+        MarkerOptions marker = new MarkerOptions().position(
+                new LatLng(latitude, longitude)).title("Hello Maps");
+
+        // Changing marker icon
+        marker.icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+          mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        // adding marker
+        googleMap.addMarker(marker);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(latitude,longitude)).zoom(14).build();
+        googleMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+
+        final Button btnCarRepaire = (Button)getView().findViewById(R.id.btnCarRepair);
+        final Button btnFuelStation = (Button)getView().findViewById(R.id.btnFuelStation);
+        mMap.setOnMyLocationClickListener(new GoogleMap.OnMyLocationClickListener() {
+            @Override
+            public void onMyLocationClick(@NonNull Location location) {
+                Log.d("onClick","MapClick");
+                mMap.clear();
+                String url = getUrl(latitude, longitude,FuelStation);
+                Object[] DataTransfer = new Object[2];
+                DataTransfer[0] = mMap;
+                DataTransfer[1] = url;
+                Log.d("onClick", url);
+                GetNearbyPlacesData getNearbyPlacesData = new GetNearbyPlacesData();
+                getNearbyPlacesData.execute(DataTransfer);
+                Toast.makeText(getContext(),"Nearby Gas Stations", Toast.LENGTH_LONG).show();
+
+
+            }
+        });
+
+        btnFuelStation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("onClick", "Button is Clicked");
+                mMap.clear();
+                String url = getUrl(latitude, longitude, FuelStation);
+                Object[] DataTransfer = new Object[2];
+                DataTransfer[0] = mMap;
+                DataTransfer[1] = url;
+                Log.d("onClick", url);
+                GetNearbyPlacesData getNearbyPlacesData = new GetNearbyPlacesData();
+                getNearbyPlacesData.execute(DataTransfer);
+                Toast.makeText(getContext(),"Nearby Fuel Stations", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnCarRepaire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("onClick", "Button is Clicked");
+                mMap.clear();
+                String url = getUrl(latitude, longitude, CarRepaire);
+                Object[] DataTransfer = new Object[2];
+                DataTransfer[0] = mMap;
+                DataTransfer[1] = url;
+                Log.d("onClick", url);
+                GetNearbyPlacesData getNearbyPlacesData = new GetNearbyPlacesData();
+                getNearbyPlacesData.execute(DataTransfer);
+                Toast.makeText(getContext(),"Nearby Car Repair Shops", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
+    }
+
+
+    private String getUrl(double latitude, double longitude, String nearbyPlace) {
+
+        StringBuilder googlePlacesUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        googlePlacesUrl.append("location=" + latitude + "," + longitude);
+        googlePlacesUrl.append("&radius=" + PROXIMITY_RADIUS);
+        googlePlacesUrl.append("&type=" + nearbyPlace);
+        googlePlacesUrl.append("&sensor=true");
+        googlePlacesUrl.append("&key=" + "AIzaSyATuUiZUkEc_UgHuqsBJa1oqaODI-3mLs0");
+        Log.d("getUrl", googlePlacesUrl.toString());
+        return (googlePlacesUrl.toString());
+    }
+
+
+
 }
--- app/src/main/java/com/example/rutujabanswal/pocketgarage/MainActivity.java
+++ app/src/main/java/com/example/rutujabanswal/pocketgarage/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.rutujabanswal.pocketgarage;
-import android.app.Fragment;
+
 import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -14,6 +11,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -33,7 +33,25 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+     //First of all, we will check if Google Play Services available or not in onCreate() function of MainActivity.java.
+        CheckGooglePlayServices();
+
     }
+
+    private boolean CheckGooglePlayServices() {
+        GoogleApiAvailability googleAPI = GoogleApiAvailability.getInstance();
+        int result = googleAPI.isGooglePlayServicesAvailable(this);
+        if(result != ConnectionResult.SUCCESS) {
+            if(googleAPI.isUserResolvableError(result)) {
+                googleAPI.getErrorDialog(this, result,
+                        0).show();
+            }
+            return false;
+        }
+        return true;
+    }
+
 
     @Override
     public void onBackPressed() {
@@ -80,21 +98,22 @@ public class MainActivity extends AppCompatActivity
             FragmentManager fragmentManager = getFragmentManager();
             fragmentManager.beginTransaction().replace(R.id.frameLayoutMain,fuel).commit();
         }
-        else if (id == R.id.nav_garage_near_me) {
-            // Handle the camera action
-            Toast.makeText(getApplicationContext(),"Garage Fragment Selected",Toast.LENGTH_SHORT).show();
-            findGarageNearMe findGarageNearMe = new findGarageNearMe();
-            FragmentManager fragmentManager = getFragmentManager();
-            fragmentManager.beginTransaction().replace(R.id.frameLayoutMain,findGarageNearMe).commit();
-        }
+
         else if (id == R.id.nav_find_nearby_garage) {
             // Handle the camera action
-            Toast.makeText(getApplicationContext(),"Garage new Fragment Selected",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(),"Garage Fragment Selected",Toast.LENGTH_SHORT).show();
             FindNearbyGarage findNearbyGarage = new FindNearbyGarage();
             android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
             fragmentManager.beginTransaction().replace(R.id.frameLayoutMain,findNearbyGarage).commit();
         }
 
+        else if (id == R.id.nav_services) {
+            // Handle the camera action
+            Toast.makeText(getApplicationContext(),"Services Fragment Selected",Toast.LENGTH_SHORT).show();
+            GarageServices services = new GarageServices();
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.frameLayoutMain,services).commit();
+        }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
